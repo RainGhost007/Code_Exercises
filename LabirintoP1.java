a20330855c5aeb70f61026aa9a7fc4ffb76605a3
rainghost007 <diogoluizago@gmail.com>
2025-07-11 14:21:13 -0300

    Adicionei obs em LabirintoP1, e Adicionei o metodo de BFS no LabirintoP2

--- LabirintoP1.java
+++ LabirintoP1.java
@@ -31,30 +31,29 @@ public class LabirintoP1 {
     		}
 		}
 	}
-	public static boolean caminho(String[] vetor, int y, int x){
+	public static boolean caminho(String[] vetor, int a, int b){ // método DFS
 		int tam = vetor.length, tamS = vetor[0].length();
 		
-		
-		if(y < 0 || y >= tam || x < 0 || x >= tamS ){return false;}
-		if(vetor[y].charAt(x) == 'F'){return true;}
-		if(vetor[y].charAt(x) == '#' || vetor[y].charAt(x) == 'X'){return false;}
-			
-		StringBuilder sb = new StringBuilder(vetor[y]);
-		sb.setCharAt(x, 'X');
-        vetor[y] = sb.toString();
-		
+		if(b < 0 || b >= tam || a < 0 || a >= tamS ){return false;} // não ultrapassar o tamanho do labirinto
+		if(vetor[b].charAt(a) == 'F'){return true;} // final
+		if(vetor[b].charAt(a) == '#' || vetor[b].charAt(a) == 'X'){return false;} // não repetir caminho ou passar por paredes
+
+		StringBuilder sb = new StringBuilder(vetor[b]);
+		sb.setCharAt(a, 'X'); // mostrar que ja foi passado por ali
+        vetor[b] = sb.toString();
+
 		for (String linha : vetor) {
-        		System.out.println(linha);
+        		System.out.println(linha); // para demonstrar no terminalo o caminho
     		}
-		
-		if(caminho(vetor, y, x+1) == true){return true;}
-		if(caminho(vetor, y+1, x) == true){return true;}
-		if(caminho(vetor, y, x-1) == true){return true;}
-		if(caminho(vetor, y-1, x) == true){return true;}
-		
-		sb.setCharAt(x, '.');
-    	vetor[y] = sb.toString();
-		
+
+		if(caminho(vetor, a, b+1) == true){return true;}
+		if(caminho(vetor, a+1, b) == true){return true;} // base do DFS, caminho que o metodo passa
+		if(caminho(vetor, a, b-1) == true){return true;}
+		if(caminho(vetor, a-1, b) == true){return true;}
+
+		sb.setCharAt(a, '.');	// retira o X e coloca . em caminhos que foram passados mas não chega em lugar nenhum
+    	vetor[b] = sb.toString(); 
+
 		return false;
 	}
 }
--- LabirintoP2.java
+++ LabirintoP2.java
@@ -2,15 +2,20 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
 
 
 public class LabirintoP2 {
+	private int x;
+	private int y;
+	private int passos;
+	private boolean visitado = false;
 	public static void main(String[] args) {
 		
 		String[] vetor = new String[201];
 		int w = 0;
 		String filePath = "input-LabirintoP2.txt";
-		try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){ // leitor do arquivo
+		try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){ // leitor do input
 			System.out.println("Este arquivo existe!");
 			String line;
 			while((line = reader.readLine()) != null){
@@ -25,33 +30,63 @@ public class LabirintoP2 {
 		catch(IOException e){
 			System.out.println("Houve um problema!");
 		}
-		int cont = 0;
-		if(caminho(vetor, 0, 0,cont)){
-			for (String linha : vetor) {
-        		System.out.println(linha);
-    		}
-			System.out.println(cont);
+
+		String[] vetorAux = new String[vetor.length]; // criei outro input para trabalhar no BFS, para encontrar o numero de passos que o DFS precisa estar limitado
+		for (int i = 0; i < vetor.length; i++) {
+    		vetorAux[i] = vetor[i];
+		}		
+
+		LinkedList<LabirintoP2> fila = new LinkedList();
+		fila.add(new LabirintoP2(0, 0, 0));
+		while(vetorAux[fila.getFirst().getX()].charAt(fila.getFirst().getY()) != 'F'){ // enquanto não chegar no final -- loop
+			int um = fila.getFirst().getX(), dois = fila.getFirst().getY(), tres = fila.getFirst().getPassos(); 
+			if(um < vetorAux.length -1){if(vetorAux[um+1].charAt(dois) == '.' || vetorAux[um+1].charAt(dois) == 'F'){fila.add(new LabirintoP2(um+1, dois, tres+1));}}		// passar por todos os caminhos possíveis
+			if(dois < vetorAux[um].length() -1){if(vetorAux[um].charAt(dois+1) == '.' || vetorAux[um].charAt(dois+1) == 'F'){fila.add(new LabirintoP2(um, dois+1, tres+1));}}
+			if(um > 0){if(vetorAux[um-1].charAt(dois) == '.' || vetorAux[um-1].charAt(dois) == 'F'){fila.add(new LabirintoP2(um-1, dois, tres+1));}}
+			if(dois > 0){if(vetorAux[um].charAt(dois-1) == '.' || vetorAux[um].charAt(dois-1) == 'F'){fila.add(new LabirintoP2(um, dois-1, tres+1));}}
+			
+			StringBuilder sb = new StringBuilder(vetorAux[um]); // utilizei o SB para mudar de . para X para que não repita o caminho ao inves de criar um objeto com algum atributo para dizer que ja foi passado por ali
+			sb.setCharAt(dois, 'X');
+        	vetorAux[um] = sb.toString();
+			fila.removeFirst();
 		}
+		int pa = fila.getFirst().getPassos(); // pega o número de passos
+		
+		System.out.println(caminho(vetor, 0, 0, pa)); // chama o metod de DFS
 	}
-	public static boolean caminho(String[] vetor, int y, int x, int cont){
+	public LabirintoP2(int x, int y, int passos) { // metodo construtor, utilizado no linkedList, para utilizar os atributos de coluna, linha e passos
+		this.x = x;
+		this.y = y;
+		this.passos = passos;
+	}
+	public int getX() {return x;}
+	public int getY() {return y;}
+	public int getPassos() {return passos;}
+	
+	public static boolean caminho(String[] vetor, int a, int b, int passos){ // método DFS
 		int tam = vetor.length, tamS = vetor[0].length();
 		
-		if(y < 0 || y >= tam || x < 0 || x >= tamS ){return false;}
-		if(vetor[y].charAt(x) == 'F'){return true;}
-		if(vetor[y].charAt(x) == '#' || vetor[y].charAt(x) == 'X'){return false;}
-			
-		StringBuilder sb = new StringBuilder(vetor[y]);
-		sb.setCharAt(x, 'X');
-        vetor[y] = sb.toString();
-		
-		if(caminho(vetor, y, x+1,cont+1) == true){return true;}
-		if(caminho(vetor, y+1, x,cont+1) == true){return true;}
-		if(caminho(vetor, y, x-1,cont+1) == true){return true;}
-		if(caminho(vetor, y-1, x,cont+1) == true){return true;}
-		
-		sb.setCharAt(x, '.');
-    	vetor[y] = sb.toString();
-		
+		if(passos < 0){return false;} // limita o número de passos que possa dar
+		if(b < 0 || b >= tam || a < 0 || a >= tamS ){return false;} // não ultrapassar o tamanho do labirinto
+		if(vetor[b].charAt(a) == 'F'){return true;} // final
+		if(vetor[b].charAt(a) == '#' || vetor[b].charAt(a) == 'X'){return false;} // não repetir caminho ou passar por paredes
+
+		StringBuilder sb = new StringBuilder(vetor[b]);
+		sb.setCharAt(a, 'X'); // mostrar que ja foi passado por ali
+        vetor[b] = sb.toString();
+
+		for (String linha : vetor) {
+        		System.out.println(linha); // para demonstrar no terminalo o caminho
+    		}
+
+		if(caminho(vetor, a, b+1,passos-1) == true){return true;}
+		if(caminho(vetor, a+1, b,passos-1) == true){return true;} // base do DFS, caminho que o metodo passa
+		if(caminho(vetor, a, b-1,passos-1) == true){return true;}
+		if(caminho(vetor, a-1, b,passos-1) == true){return true;}
+
+		sb.setCharAt(a, '.');	// retira o X e coloca . em caminhos que foram passados mas não chega em lugar nenhum
+    	vetor[b] = sb.toString(); 
+
 		return false;
 	}
 }
