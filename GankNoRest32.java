d7624782bed59cce9ec5a81b45f46792f85b09bf
rainghost007 <diogoluizago@gmail.com>
2025-08-16 00:46:55 -0300

    Modificacao no ganknorest32 parte2

--- GankNoRest32.java
+++ GankNoRest32.java
@@ -1,39 +1,43 @@
-import java.util.ArrayList;
 
 public class GankNoRest32 {
-    private int idade;
-    private int vezes;
+    private long idade;
+    private long vezes;
     public static void main(String[] args) {
-        ArrayList<Integer> input = new ArrayList<>();
-        ArrayList<GankNoRest32> aux = new ArrayList<>();
-        input.add(2);
-        input.add(4);
-        input.add(1);
-        input.add(5);
-        input.add(2);
-        input.add(3);
-        for (int i = 0; i < 20; i++) {
-            int tam = input.size();
-            if(tam%2 == 1){input.removeLast();}
-            for(int j = 1; j < tam; j+=2){
-                int idade = input.get(j);
-                int vezes = input.get(j-1);
-                aux.add(new GankNoRest32(idade, vezes));
-            }
-            input.clear();
-            for (int k = 0; k < aux.size(); k++) {
-                for (int l = 0; l < aux.get(k).getVezes(); l++) {
-                    input.add(aux.get(k).getIdade());
-                }
-            }
-            aux.clear();
+        
+        int[] input = {2,4,1,5,2,3};
+        
+        GankNoRest32[] aux = new GankNoRest32[input.length/2];
+
+        for (int i = 0, k = 1; i < aux.length; i++, k+=2) {
+            aux[i] = new GankNoRest32(input[k],input[k-1]);
         }
-        System.out.println(input.size());
+        long tam = aux.length, result = 0;
+        for (int j = 0; j < 59; j++) {
+            long antes = 0;
+            for (int l = 0; l < tam; l++) {
+                long vez = aux[l].getVezes();
+                long idad = aux[l].getIdade();
+                if(l == tam - 1) aux[l].setVezes(vez-1);
+                if(antes%2 == 1){
+                    antes = vez;
+                    aux[l].setVezes((((vez-1)/2)*idad)+aux[l-1].getIdade());
+                }
+                    else{
+                        antes = vez;
+                        aux[l].setVezes((vez/2)*idad);
+                    }
+            }
+        }
+        for (GankNoRest32 aux1 : aux) {
+            result += aux1.getVezes();
+        }
+        System.out.println(result);
     }
-    public GankNoRest32(int idade, int vezes){
+    public GankNoRest32(long idade, long vezes){
         this.idade=idade;
         this.vezes=vezes;
     }
-    public int getIdade(){return idade;}
-    public int getVezes(){return vezes;}
+    public long getIdade(){return idade;}
+    public long getVezes(){return vezes;}
+    public void setVezes(long vezes){this.vezes=vezes;}
 }
